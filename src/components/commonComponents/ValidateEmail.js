8da745e3199f86987bd15ee1efda8959cd2948f0
Endrina Toledo <71093001+endrina-toledo@users.noreply.github.com>
2022-05-18 20:28:50 -0400

    ajuste en la validacion de formato de correo

--- src/components/commonComponents/ValidateEmail.js
+++ src/components/commonComponents/ValidateEmail.js
@@ -1,10 +1,14 @@
-import React from 'react'
 
-const ValidateEmail = ({email}) => {
+const ValidateEmail = (email) => {
+    if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email)){
+        return true;
+       } else {
+        return false;
+       }
 
-    const re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    // const re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
 
-    return (!re.exec(email)) ? false : true
+    // return (!re.exec(email)) ? false : true
 }
 
 export default ValidateEmail
