a2a3d900236b18feae4d24b5b1c9507e5fb1a113
Endrina Toledo <37427058+endrinatoledo@users.noreply.github.com>
2022-09-08 10:12:48 -0400

    ajustes por revisar desde la escuela

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ app.use(
       changeOrigin: true,
     })
   );
-app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../build', 'index.html'));
+// });
 
 const port = process.env.PORT || 3000
 app.listen(port);
