eb6b3c394e040aeba863fe642ee4773f78d2368b
Endrina Toledo <etoledo@wau.com>
2022-11-12 18:41:21 -0400

    formato de factura avance

--- src/components/Payments/ComprobantePDF.js
+++ src/components/Payments/ComprobantePDF.js
@@ -32,13 +32,18 @@ const styles = StyleSheet.create({
 
 const ComprobantePDF = () => {
 
+    //PULGADAS : 9.5  - 5.5
+    //CENTIMETROS: 24.13 - 13.97
+    //MILIMETROS: 228.6 - 139.7
+
     return (
         <PDFViewer style={styles.viewer}>
             {/* Start of the document*/}
             <Document>
                 {/*render a single page*/}
-                <Page size="A4" style={styles.page}>
-                    <View style={{ margin:'15px'
+                <Page size="A2" style={styles.page}>
+                    <View style={{
+                        marginTop: '75px', marginLeft: '75px', marginRight: '75px', marginBottom:'75px'
                         // paddingTop:"15px", paddingRight:'15px', paddingLeft:'15px'
                         }}>
                         <View style={{ display: "flex", flexDirection:'row' }}>
@@ -80,16 +85,45 @@ const ComprobantePDF = () => {
                             </View>
                         </View>   
                         <View style={{ height: '19px' }}></View>
-                        <View style={{ display: "flex", flexDirection: 'row' }}>  
+                        <View style={{ display: "flex", flexDirection: 'row', border:'1', padding:'10px'}}>  
                             <View style={{ flex: 3 }}>
                                 <View >
                                     <Text style={{ fontSize: tamañoLetra, textAlign: 'center' }}> CONCEPTO </Text>
                                 </View>
                             </View> 
                             <View style={{ flex: 1 }}>
-                                <Text style={{ fontSize: tamañoLetra, textAlign: 'center' }}> BOLÍVARES </Text>
+                                <Text style={{ fontSize: tamañoLetra, textAlign: 'right' }}> BOLÍVARES </Text>
                             </View> 
-
+                        </View>
+                        <View style={{ height: '19px' }}></View>
+                        <View style={{ display: "flex", flexDirection: 'row' }}>  
+                            <View style={{ flex: 3 }}>
+                                <View >
+                                    <Text style={{ fontSize: tamañoLetra, textAlign: 'left' }}> conceptos </Text>
+                                </View>
+                            </View>
+                            <View style={{ flex: 1 }}>
+                                <Text style={{ fontSize: tamañoLetra, textAlign:'right' }}> bolivares </Text>
+                            </View> 
+                        </View>
+                        <View style={{ height: '19px' }}></View>
+                        <View style={{ display: "flex", flexDirection: 'row', border: '1' }}>
+                            <View style={{ flex: 3, borderRight: '1', margin:'1px'  }}>
+                                <View >
+                                    <Text style={{ fontSize: tamañoLetra, textAlign: 'left' }}> Formas de pago </Text>
+                                </View>
+                            </View>
+                            <View style={{ flex: 1 }}>
+                                <Text style={{ fontSize: tamañoLetra, textAlign: 'right' }}> total </Text>
+                            </View>
+                        </View>
+                        <View style={{ height: '19px' }}></View>
+                        <View style={{ display: "flex", flexDirection: 'row' }}>
+                            {/* <View style={{ flex: 3 }}> */}
+                                <View >
+                                    <Text style={{ fontSize: tamañoLetra, textAlign: 'left' }}> Conforme de recibido: _______________________________________ </Text>
+                                </View>
+                            {/* </View> */}
                         </View>
                     </View>
                 </Page>
